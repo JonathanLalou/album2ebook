dcae33a42fbe33cf4992a45cf756ab168974e0a6
JonathanLalou <jonathan.lalou@gmail.com>
2020-04-07 12:37:27 -0400

    step commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # album2ebook
 Generates an ebook from folder(s) of pictures
+
+# Initial POM
+https://start.spring.io/#!type=maven-project&language=groovy&platformVersion=2.3.0.M4&packaging=jar&jvmVersion=11&groupId=com.github.jonathanlalou&artifactId=album2book&name=album2book&description=Image%20to%20ePub%20script&packageName=com.github.jonathanlalou.album2book&dependencies=lombok,devtools,configuration-processor,actuator
--- src/main/groovy/com/github/jonathanlalou/album2book/Album2Epub.groovy
+++ src/main/groovy/com/github/jonathanlalou/album2book/Album2Epub.groovy
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
 import javax.annotation.PostConstruct
+import java.nio.file.Files
+import java.nio.file.Path
 
 @Component
 @Getter
@@ -16,6 +18,10 @@ import javax.annotation.PostConstruct
 @Log4j
 class Album2Epub implements ApplicationRunner {
 
+    @Value('${work.folder}')
+    String workFolder
+    @Value('${source.folder}')
+    String sourceFolder
     @Value('${ebook.target.type}')
     String targetType
     @Value("#{'\${chapter.folders}'.split(',')}")
@@ -25,6 +31,7 @@ class Album2Epub implements ApplicationRunner {
 
     @PostConstruct
     void postConstruct() {
+        log.info("sourceFolder: $sourceFolder")
         log.info("targetType: $targetType")
         log.info("folders: ${folders}")
         log.info("titles: ${titles}")
@@ -32,6 +39,31 @@ class Album2Epub implements ApplicationRunner {
 
     @Override
     void run(ApplicationArguments args) throws Exception {
+        final File work = new File(workFolder)
+        if (work.exists()) {
+            work.deleteDir()
+        }
+        work.mkdirs()
+        copyResource("epub/mimetype", "mimetype")
+        copyResource("epub/container.xml", "/META-INF/container.xml")
+        copyResource("epub/styles.css", "/OEBPS/Styles/styles.css")
+
         log.warn("Hello world")
+        for (int i = 0; i < folders.size(); i++) {
+            log.info("Chapter: $i *** ${folders[i]} *** ${titles[i]}")
+            File folder = new File(sourceFolder + folders[i])
+            if (folder.exists()) {
+                final List<File> files = Arrays.asList(folder.listFiles())
+                for (File file : files) {
+                    log.info(file)
+                }
+            }
+        }
+    }
+
+    def Path copyResource(String sourceFile, String targetFile) {
+        Path targetPath = Path.of("${workFolder}/" + targetFile)
+        Files.createDirectories(targetPath.getParent());
+        Files.copy(Path.of("./src/main/resources/" + sourceFile), targetPath)
     }
 }
