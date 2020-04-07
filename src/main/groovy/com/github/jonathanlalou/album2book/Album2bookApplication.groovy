d8dcc098ae11e5f8465d6ba53714f4067bb3529d
JonathanLalou <jonathan.lalou@gmail.com>
2020-04-07 08:40:56 -0400

    init commit

---
+++ src/main/groovy/com/github/jonathanlalou/album2book/Album2bookApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.jonathanlalou.album2book
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Album2bookApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(Album2bookApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/github/jonathanlalou/album2book/Album2bookApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.jonathanlalou.album2book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Album2bookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
